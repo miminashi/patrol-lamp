d30a4c6d30109f3a2ff3cce6744a6bec5d16e21e
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2016-03-15 09:06:30 +0900

    Uses streaming API directly instead of IFTTT

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
 import time
 import mraa
-import yaml
-import paho.mqtt.client as mqtt
+from twitter import *
 
-CA_CERTS_FILE = "/etc/ssl/certs/ca-certificates.crt"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+SCREEN_NAME = ""
 
-with open(".env.example", "r") as f:
-    dic = yaml.load(f)
-    HOST = dic.get("HOST")
-    PORT = dic.get("PORT")
-    USERNAME = dic.get("USERNAME")
-    PASSWORD = dic.get("PASSWORD")
+print "hello"
 
 gpio = mraa.Gpio(13)
 gpio.dir(mraa.DIR_OUT)
 
-def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("honban/pat-lamp")
+auth = OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter_stream = TwitterStream(auth=auth, domain="userstream.twitter.com")
+for msg in twitter_stream.user():
+    if "in_reply_to_screen_name" in msg and "text" in msg:
+        if msg["in_reply_to_screen_name"] == SCREEN_NAME:
+            print msg["text"]
+            for i in xrange(3):
+                gpio.write(1)
+                time.sleep(1.0)
+                gpio.write(0)
+                time.sleep(0.5)
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-  
-    gpio.write(1)
-    time.sleep(3)
-    gpio.write(0)
-
-
-if __name__ == '__main__':
-    client = mqtt.Client(protocol=mqtt.MQTTv311)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.tls_set(CA_CERTS_FILE)
-    client.username_pw_set(USERNAME, PASSWORD)
-    client.connect(HOST, PORT, 60)
-    client.loop_forever()
